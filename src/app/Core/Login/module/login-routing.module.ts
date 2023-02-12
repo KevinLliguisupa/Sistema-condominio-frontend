2e2e549838c4e83a66d0479fa0f103662b2dfc81
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2023-02-12 07:26:51 -0500

    Fix: Ruteo y eliminacion de archivos innecesarios

--- src/app/Core/Login/module/login-routing.module.ts
+++ src/app/Core/Login/module/login-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FinMontoComponent } from 'src/app/Financiero/components/finMonto/finMonto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'monto',component: FinMontoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/Core/Login/module/login.module.ts
+++ src/app/Core/Login/module/login.module.ts
@@ -5,12 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './login-routing.module';
 import { LoginComponent } from '../components/login.component';
 import { FormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   declarations: [LoginComponent],
   providers: [],
