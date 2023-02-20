105cdc551171ebff94be17219e6da789492816c2
Kevin Vinueza <kxvinuezac@utn.edu.ec>
2023-02-20 15:39:30 -0500

    Funcionalidad completa para ver el ingreso de dinero como para generar pagos de servicios para solucionar incidencias del condominio

--- src/app/Financiero/components/finGastos/finGastos.component.ts
+++ src/app/Financiero/components/finGastos/finGastos.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FinTipoServicioService } from '../../services/finTipoServicio/finTipoServicio.service';
+import { FinTipoServicioModel } from '../../models/finTipoServicio.model';
+import { FinIncidenciaModel } from '../../models/finIncidencia.model';
+import { FinIncidenciaService } from '../../services/finIncidencia/finIncidencia.service';
+import { FinGastosModel } from '../../models/finGastos.model';
+import { FinGastosService } from '../../services/finGastos/finGastos.service';
+import { FormBuilder,FormGroup } from '@angular/forms';
+import { FinImagenModel } from '../../models/finImagen.model';
 @Component({
   selector: 'app-finGastos',
   templateUrl: './finGastos.component.html',
@@ -7,9 +14,156 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinGastosComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  public imagen!:File;
+  public form!: FormGroup;
+  public serviciosList : FinTipoServicioModel[]=[]
+  public incidenciasList: FinIncidenciaModel[]=[];
+  public gastosList: FinGastosModel[]=[]
+
+
+  public serviciosMap = new Map();
+  public incidenciasMap= new Map();
+  public recibosMap= new Map();
+
+  public imagenMostrar="";
+  public idPago=0;
+
+
+  constructor(private formBuilder:FormBuilder, 
+    private finGastosService: FinGastosService,
+    private finTipoServiciosService: FinTipoServicioService,
+    private finIncidenciaService:FinIncidenciaService) { }
+
+  ngOnInit( ) {
+    this.getAllTipoServicios();
+    this.getAllInidenciasAbiertas();
+    this.getAllGastos();
+    this.getAllRecibos();
+    this.form=this.formBuilder.group({
+      txtIncidencia:[''],
+      txtServicio:[''],
+      txtPago:[0]
+    })
   }
 
+
+  public getAllTipoServicios(){
+    this.finTipoServiciosService.getTiposServicios().subscribe(
+      (servicio:any)=>{
+        var serviciosAux:FinTipoServicioModel[]=servicio;
+        serviciosAux.forEach((p)=>{
+          if(p.tseIncidencia){
+            this.serviciosList.push(p);
+            this.serviciosMap.set(p.tseId,p.tseNombre);
+          }
+        });     
+      }
+    );
+  }
+
+  public getAllInidenciasAbiertas(){
+    this.finIncidenciaService.getAllIncidenciasNoSolucionadas().subscribe(
+      (incidencia:any)=>{
+        this.incidenciasList=incidencia;
+        this.incidenciasList.forEach((p)=>{
+          this.incidenciasMap.set(p.incId,p.incDescripcion);
+        });
+        
+      }
+    );
+  }
+
+  public getAllGastos(){
+    this.finGastosService.getAllGastos().subscribe(
+      (gasto:any)=>{
+        this.gastosList=gasto;
+      }
+    );
+  }
+
+  public getAllRecibos(){
+    this.finGastosService.getAllRecibos().subscribe(
+      (recibos:any)=>{
+        var recibosList:FinImagenModel[]= recibos;
+        recibosList.forEach((p)=>{
+          this.recibosMap.set(p.id,p.imagenBinarizada);
+        })
+    }
+    );
+  }
+
+  public selectImagen(id:any){
+    this.imagenMostrar= this.recibosMap.get(id);
+  }
+
+  public mostrar(){
+    return this.imagenMostrar;
+  }
+
+  public getIncidenciaById(id:any){
+    return this.incidenciasMap.get(id);
+  }
+  public getServicioById(id:any){
+    return this.serviciosMap.get(id);
+  }
+
+  public normalizarFecha(fecha:any){
+    const fechaObjeto= new Date(fecha);
+    const anio = fechaObjeto.getFullYear();
+    const mes = fechaObjeto.getMonth() + 1; 
+    const dia = fechaObjeto.getDate();
+    const fechaFormateada = anio + '/' + (mes < 10 ? '0' + mes : mes) + '/' + (dia < 10 ? '0' + dia : dia);
+    return fechaFormateada;
+
+  }
+
+
+  public cargarImagen(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length) {
+      this.imagen = input.files[0];
+    }
+  }
+  
+  public postGasto(event: Event): void {
+
+    event.preventDefault();
+    if (this.imagen && this.form.value.txtPago>0 && this.form.value.txtIncidencia!='' && this.form.value.txtServicio!='') {
+    
+      var pago={
+        tseId:this.form.value.txtServicio,
+        incId:this.form.value.txtIncidencia,
+        gasCedTesorero:'1728848373',
+        gasPago:this.form.value.txtPago,
+        gasRecibo:this.imagen 
+      }
+      this.finGastosService.postGasto(pago)
+        .subscribe(resultado => {
+          console.log('Se ha ingresado correctamente:', resultado);
+        });
+    }
+    
+  }
+
+  public getDatos(id:any){
+    this.idPago=id;
+  }
+
+  public updateGasto(event: Event): void {
+
+    event.preventDefault();
+    if (this.imagen) {
+    
+      var pago={
+        gasRecibo:this.imagen 
+      }
+      this.finGastosService.putGasto(pago,this.idPago)
+        .subscribe(resultado => {
+          console.log('Se ha actualizado correctamente:', resultado);
+        });
+    }
+    
+  }
+  
 }
--- src/app/Financiero/services/finGastos/finGastos.service.ts
+++ src/app/Financiero/services/finGastos/finGastos.service.ts
@@ -1,10 +1,65 @@
-import { Injectable } from '@angular/core';
-
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { fromEvent } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class FinGastosService {
+  @Output() disparador: EventEmitter<any> = new EventEmitter();
+constructor(private http: HttpClient) { }
 
-constructor() { }
+
+public getAllGastos(){
+  const url =`http://localhost:8080/web/api/rest/v1/financiero/gasto`;
+  return this.http.get(url);
+}
+
+public getAllRecibos(){
+  const url =`http://localhost:8080/web/api/rest/v1/financiero/gasto/recibos`;
+  return this.http.get(url);
+
+}
+
+public getAllGastosByIncidencia(id:any){
+  const url =`http://localhost:8080/web/api/rest/v1/financiero/gasto/incidencia/`+id;
+  return this.http.get(url);
+}
+
+public postGasto(gastoModel: any){
+  const reader = new FileReader();
+  var imagen=gastoModel.gasRecibo;
+  reader.readAsDataURL(imagen);
+  return fromEvent(reader, 'load').pipe(
+      map(() => reader.result as string),
+      switchMap((base64: string) => {
+         
+        var pago={
+          tseId:gastoModel.tseId,
+          incId:gastoModel.incId,
+          gasCedTesorero:gastoModel.gasCedTesorero,
+          gasPago:gastoModel.gasPago,
+          gasRecibo:base64 
+        }
+         return this.http.post<string>(`http://localhost:8080/web/api/rest/v1/financiero/gasto`, pago);
+      })
+  );
+}
+
+public putGasto(gastoModel: any, id:any){
+    const reader = new FileReader();
+    var imagen=gastoModel.gasRecibo;
+    reader.readAsDataURL(imagen);
+    return fromEvent(reader, 'load').pipe(
+        map(() => reader.result as string),
+        switchMap((base64: string) => {
+           
+          var pago={
+            gasRecibo:base64
+          }
+           return this.http.put<string>(`http://localhost:8080/web/api/rest/v1/financiero/gasto/`+id, pago);
+        })
+    );
+}
 
 }
